4c76f1eed1664a326962cc7b860ed5c706dcbba0
sergi1307 <sergi13072006@gmail.com>
2025-02-24 08:46:22 +0100

    Añadimos clase Oficina con sus constructores, getters y setters

---
+++ Oficina.java
@@ -0,0 +1,25 @@
+public class Oficina extends Articulo {
+    private String descripcion;
+
+    /*Constructor por defecto*/
+    public Oficina() {
+        super();
+        this.descripcion = "";
+    }
+
+    /*Constructor General*/
+    public Oficina(String codA, double pre, int iva, int stc, String desc) {
+        super(codA, pre, iva, stc);
+        this.descripcion = desc;
+    }
+
+    /*Getters*/
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    /*Setters*/
+    public void setDescripcion(String s) {
+        this.descripcion = s;
+    }
+}
